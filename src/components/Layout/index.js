952f201fff965e93b1b9746fd554150a31c1da68
Sharlet Wilson <sharlet.wilson@gmail.com>
2019-01-08 01:19:07 +0530

    Added drawer handlers

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer';
 
-const Layout = (props) => {
-    return(
-        <>
-            <Toolbar />
-            <main className={classes.Content}>{props.children}</main>
-        </>
-    )
+class Layout extends Component {
+    constructor() {
+        super();
+        this.state = {
+            open: false
+        }
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({ open: false })
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState(state => {
+            return { open: !state.open }
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.open} closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>{this.props.children}</main>
+            </>
+        )
+    }
 }
 
 export default Layout;
